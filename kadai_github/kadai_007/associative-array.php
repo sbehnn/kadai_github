8a1321995db560e36057c481c4fee4e47779c3a0
ShimamuraHinano <saiga3187@gmail.com>
2025-02-19 18:53:50 +0900

    Add files via upload

---
+++ kadai_github/kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP課題007</title>
+</head>
+
+<body>
+   <p>
+   <?php
+
+       $personal_data = ['name' => 'onion', 'price' => 200, 'wight' => 160];
+
+       print_r($personal_data);
+
+       ?>
+   </p> 
+</body>
+
+</html>
